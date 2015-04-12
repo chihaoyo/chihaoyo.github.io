c1019b871d6dd749e02ac0685f93d77e7e299fde
chihaoyo <chihaoyo@gmail.com>
2015-04-11 22:26:42 -0400

    Try layout

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 var _root = new Firebase('https://youzhihao.firebaseio.com/');
 var _projects = _root.child('projects');//.push({id: 0, title: 'try'});
+
+$('#categories > .category > a').click(function(event) {
+	$('#projects').removeClass('hide');
+	event.stopPropagation();
+});
+$('article').click(function() {
+	$('#projects').addClass('hide');
+});
